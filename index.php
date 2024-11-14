907fdfcf0288cc54e072a8d979b438ddba6c2ae9
Thibault-Franceus <r1006946@student.thomasmore.be>
2024-11-14 12:46:35 +0100

    .

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
     include_once(__DIR__ . '/Classes/Db.php');
     include_once(__DIR__ . '/Classes/Product.php');
 
-    session_start();
-    if($_SESSION['loggedin'] !== true){
-    header('location: login.php');
-  }
-
     $conn = Db::getConnection();
     $stmt = $conn->query('SELECT * FROM products');
     $stmt->execute();
@@ -22,25 +17,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bikeshop</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
     <h1>webshop</h1>
-    <ul>
+    <div class="webshop">
         <?php foreach($products as $product): ?>
-            <div class="card">
+        <div class="card">
 
-  <div class="imgBox">
-    <img src="" alt="mouse corsair" class="mouse">
-  </div>
+            <div class="imgBox">
+                <img src="https://media.trekbikes.com/image/upload/w_1200/CheckpointALR3-25-47530-A-Primary" alt="mouse corsair" class="mouse">
+            </div>
 
-  <div class="contentBox">
-    <h3>Mouse Corsair M65</h3>
-    <h2 class="price">61.<small>98</small> €</h2>
-    <a href="#" class="buy">Buy Now</a>
-  </div>
-
-</div> 
+            <div class="contentBox">
+                <h3><?php echo $product['Title']; ?></h3>
+                <h2 class="price"><?php echo $product['Price']; ?> €</h2>
+                <a href="#" class="buy">Buy Now</a>
+            </div>
+        </div> 
         <?php endforeach; ?>
+
+
+    </div>
     
 </body>
 </html>
