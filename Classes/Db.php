684e34b1b3b81a2515378574d495aac1de4a2c3a
Thibault-Franceus <r1006946@student.thomasmore.be>
2024-11-20 20:31:41 +0100

    feat: we blijven proberen

--- Classes/Db.php
+++ Classes/Db.php
@@ -1,17 +1,15 @@
 <?php 
-    class Db {
 
-        public static function getConnection(){
             $pathToSSL = __DIR__ . '/cacert.pem';
-    $options = array(
-        PDO::MYSQL_ATTR_SSL_CA => $pathToSSL
-    );
+            $options = array(
+            PDO::MYSQL_ATTR_SSL_CA => $pathToSSL
+            );
 
     $host = 'bikeshopweb.mysql.database.azure.com';
     $db = 'bikeshop';
     $user = 'trekbikes';
     $pass = '4rL#2m$9Bn@7tQv!';
-    $db = new PDO("mysql:host=$host;dbname=$db", $user, $pass, $options);
-        }
-    }
+    $db = new PDO("mysql:host=$host;port=8080;dbname=$db", $user, $pass, $options);
+        
+    
 ?>
